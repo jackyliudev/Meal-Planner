d32bf9de3f32b326cfee6cff2aa958ad4abe4eb8
Jacky Liu <jackyliudev@gmail.com>
2022-08-01 11:54:25 -0400

    Updated meal planner with new input field for ingredients

--- js/main.js
+++ js/main.js
@@ -1,36 +1,102 @@
-document.getElementById('dragSqr').addEventListener('dragstart',dragstart_handler)
-//document.getElementById('mealname').addEventListener('blur',blur_handler)
-//document.getElementById('target').addEventListener('dragover',dragover_handler)
-//document.getElementById('target').addEventListener('drop',drop_handler)
-//document.getElementById('target').addEventListener('dragexit',dragexit_handler)
+// Retrieve elements from the DOM
+const addIngBtn = document.getElementById('addingredientbtn');
+const addToCal = document.getElementById('addtocalendar');
+const ingList = document.getElementById('inglist');
 
-document.querySelectorAll('.dropzone').forEach(x=>{x.addEventListener('dragover',dragover_handler)})
-document.querySelectorAll('.dropzone').forEach(x=>{x.addEventListener('drop',drop_handler)})
-document.querySelectorAll('.dropzone').forEach(x=>{x.addEventListener('dragexit',dragexit_handler)})
+// Add eventlistener to the buttons
+addIngBtn.addEventListener('click',addNewfield);
+addToCal.addEventListener('click',addToCalendar);
 
-function dragstart_handler(ev) {
+// Keep count of number of ingredients
+let ingCt = 6;
+
+// Keep track of the currently selected meal 
+let selectedMeal = document.getElementById('bfSun');
+
+// Add Dragstart to the drag buttons
+document.getElementById('dragSqr').addEventListener('dragstart',dragstartHandler)
+
+// Add event listeners to the cells of the table
+document.querySelectorAll('.dropzone').forEach(x=>{
+    x.addEventListener('dragover',dragoverHandler);
+    x.addEventListener('drop',dropHandler);
+    x.addEventListener('dragexit',dragexitHandler);
+    x.addEventListener('click',selectMeal);
+})
+
+// Function to define the event for drag start
+function dragstartHandler(ev) {
     // Add the target element's id to the data transfer object
     ev.dataTransfer.setData("text/plain", document.getElementById('mealname').value);
     //console.log(ev.target.id)
 }
 
-function dragover_handler(ev) {
+// Function to define the event for drag over the cell
+function dragoverHandler(ev) {
     ev.preventDefault();
-    const dragOverTarget = ev.target.id;
+    const dragOverTarget = document.getElementById(ev.target.id);
     ev.dataTransfer.dropEffect = "copy";
-    document.getElementById(ev.target.id).classList.add('dragover')
+    // Remove the highlighted effect for the selected meal
+    selectedMeal.classList.remove('selectedmeal')
+
+    // Add the highlighted effect for drag over meal
+    dragOverTarget.classList.add('dragover');
 }
-function drop_handler(ev) {
+
+// Function to define the event after item is dropped in the drop zone
+function dropHandler(ev) {
     ev.preventDefault();
     // Get the id of the target and add the moved element to the target's DOM
     const data = ev.dataTransfer.getData("text/plain");
-    document.getElementById(ev.target.id).innerText = data;
-    document.getElementById(ev.target.id).classList.remove('dragover')
-    //console.log(data)
-    //ev.target.appendChild(document.getElementById(data));
+    selectedMeal = document.getElementById(ev.target.id);
+    selectedMeal.innerText = data;
+    // Remove the dragover effect and add the selected meal effect
+    selectedMeal.classList.remove('dragover');
+    selectedMeal.classList.add('selectedmeal');
 }
 
-function dragexit_handler(ev){
+// Function to define the event after exiting the drag area
+// Drop area is defined by the class dropzone in html
+function dragexitHandler(ev){
     ev.preventDefault();
-    document.getElementById(ev.target.id).classList.remove('dragover')
+    document.getElementById(ev.target.id).classList.remove('dragover');
+    selectedMeal.classList.add('selectedmeal');
+}
+
+// Function for adding new field for ingredients list
+function addNewfield(ev){
+    //Create new HTMl elements
+    const newDiv = document.createElement('div');
+    const newSpan = document.createElement('span');
+    const newInput = document.createElement('input');
+
+    // Set the attribute for the input element
+    newInput.setAttribute('type','text')
+    newInput.setAttribute('id',`ing${ingCt}`)
+    newInput.setAttribute('placeholder',`Ingredient ${ingCt}`)
+
+    // Create the Text for the ingredient input field
+    const txtContent = document.createTextNode(`Ingredient ${ingCt}: `);
+
+    // Create html element and add to the ingredient list
+    newSpan.append(txtContent)
+    newDiv.append(newSpan)
+    newDiv.append(newInput)
+    ingList.appendChild(newDiv)
+
+    // Increase the ingredient count
+    ingCt++;
+}
+
+// Function for selecting meal in the calendar
+function selectMeal(ev){
+    selectedMeal.classList.remove('selectedmeal');
+    selectedMeal = document.getElementById(ev.target.id);
+    selectedMeal.classList.add('selectedmeal');
+}
+
+// Function for the button that adds the meal to the calendar
+function addToCalendar(ev){
+    const data = document.getElementById('mealname').value;
+    selectedMeal.innerText = data;
 }
