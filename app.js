c0763a9afd0040e6cd716687a3b08b9ab2c560d0
Jacky Liu <jackyliudev@gmail.com>
2022-08-15 09:16:22 -0400

    Changed folder structure and added backend

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
+// Load Config
+dotenv.config({path: './config/config.env'})
+
+const app = express();
+
+// Handlebars
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    extname:'.hbs'
+}))
+app.set('view engine', 'hbs')
+
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes
+app.use('/',require('./routes/index'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT,
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
