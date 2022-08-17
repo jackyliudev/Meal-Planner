f360f7b427f89474a9c6bff013ca46d30d48dada
Jacky Liu <jackyliudev@gmail.com>
2022-08-17 19:52:21 -0400

    Updated nav header based on authentication

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,11 @@
 // Controls home page view
 function getIndex(req,res){
-    res.render('dashboard')
+    const logValue = req.isAuthenticated() ? 
+        '<li><a href="/login/logout">Logout</a></li>':
+        '<li><a href="/login">Login</a></li>';
+    res.render('dashboard', {
+        loginStatus: logValue
+    })
 }
 
 
